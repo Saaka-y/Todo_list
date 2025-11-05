935d1697faadbaa7dbfe18b8747d36271558a994
Sara <188415334+Saaka-y@users.noreply.github.com>
2025-11-05 15:06:59 +0900

    🚧 separeta pages into index(today's task) and upcoming tasks

--- src/pages/upcoming.jsx
+++ src/pages/upcoming.jsx
@@ -1,20 +1,20 @@
+// all tasks except today's here
 import Head from "next/head";
 import { TodoForm } from "@/components/TodoForm";
 import { Header } from "@/components/Header";
 import { ThemeToggle } from "@/components/ThemeToggle";
 import Link from "next/link";
 
-
-export default function Home() {
+export default function Upcoming() {
   return (
     <div>
       <Head>
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>Today&apos;s tasks</title>
+        <title>Upcoming tasks</title>
       </Head>
-      <Link href="/upcoming">
-        Upcoming tasks
+      <Link href="/">
+        Today&apos;s tasks
       </Link>
       <Header />
       <TodoForm />
