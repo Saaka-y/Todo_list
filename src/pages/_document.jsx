2463aba2ce903fad6986ec4d5b60a94f36f4654f
Sara <188415334+Saaka-y@users.noreply.github.com>
2025-11-10 13:42:36 +0900

    💄 delete default favicon and images and replace with new svg

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -4,6 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <link rel="icon" href="/calendar.svg" type="image/svg+xml" />
         <link
           href="https://fonts.googleapis.com/css2?family=Actor&family=Amatic+SC:wght@400;700&family=Cagliostro&family=Kirang+Haerang&display=swap"
           rel="stylesheet"
