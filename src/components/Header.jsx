a29a29d1f76660202949010dd8e3378ca8192515
Sara <188415334+Saaka-y@users.noreply.github.com>
2025-12-03 15:45:03 +0900

    🧐 review codes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,9 +3,8 @@ import { useEffect, useState } from "react";
 import { CiLight, CiDark } from "react-icons/ci";
 import Link from "next/link";
 
-// to be updated with setUser
-
 export function Header({ page, isDarkMode, setIsDarkMode }) {
+  // show a random encouraging message
   const greetings = [
     "Let's tackle your tasks today!",
     "Ready to get things done?",
@@ -23,30 +22,31 @@ export function Header({ page, isDarkMode, setIsDarkMode }) {
   }, [])
 
   return (
-    <><header className={style.header}>
-      <nav className="flex justify-between gap-x-2">
-        <button
+    <>
+      <header className={style.header}>
+        <nav className="flex justify-between gap-x-2">
+          <button
             onClick={() => setIsDarkMode(prev => !prev)}
             className={style.darkModeToggle}
           >
-            {isDarkMode ? (<CiLight size={24}/>) : (<CiDark size={24}/>)}
+            {isDarkMode ? (<CiLight size={24} />) : (<CiDark size={24} />)}
           </button>
-        <div className="flex gap-2">
-          {page === "today" ? (
-            <>
-              <Link href="/upcoming" className={style.selectableTab}>Upcoming</Link>
-              <p className={style.currentPageTab}>Today</p>
-            </>
-          ) : (
-            <>
-              <p className={style.currentPageTab}>Upcoming</p>
-              <Link href="/" className={style.selectableTab}>Today</Link>
-            </>
-          )}
-        </div>
+          <div className="flex gap-2">
+            {page === "today" ? (
+              <>
+                <Link href="/upcoming" className={style.selectableTab}>Upcoming</Link>
+                <p className={style.currentPageTab}>Today</p>
+              </>
+            ) : (
+              <>
+                <p className={style.currentPageTab}>Upcoming</p>
+                <Link href="/" className={style.selectableTab}>Today</Link>
+              </>
+            )}
+          </div>
 
-      </nav>
-    </header >
+        </nav>
+      </header >
       {page === "upcoming" ? null : (<h1
         className={`${style.greeting} text-md md:text-xl leading-relaxed`}>{message}</h1>)}
     </>
