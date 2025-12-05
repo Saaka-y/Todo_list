775dd9c34fd1f88261ffd2a048f639b5968a0c19
Sara <188415334+Saaka-y@users.noreply.github.com>
2025-12-05 19:10:30 +0900

    ♻️ refacotr with new useFilterTodos hook

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,59 +1,21 @@
+//@/components/TodoList.jsx
 import { ListMessage } from "@/components/ListMessage";
 import { TodoItem } from "@/components/TodoItem";
+import { useFilterTodos } from "@/hooks/useFilterTodos";
 
-
-export function TodoList({ todoList, setTodoList, page, filterType }) {
-
-  const toggleTask = (id) => {
-    setTodoList(prev =>
-      prev.map(task =>
-        task.id === id ? { ...task, completed: !task.completed } : task
-      )
-    );
-  }
-
-  const deleteTask = (id) => {
-    setTodoList(prev => prev.filter(task => task.id !== id));
-  }
-
-  const editTask = (id, newText, newDateString) => {
-    setTodoList(prev =>
-      prev.map(task =>
-        task.id === id ? { ...task, text: newText, date: newDateString } : task
-      )
-    );
-  }
-
-  let filteredTodoList;
-  const today = new Date().toISOString().split("T")[0];
-
-  if (filterType === "today") {
-    filteredTodoList = todoList.filter((todo) => todo.date === today && !todo.completed);
-  } else if (filterType === "upcoming") {
-    filteredTodoList = todoList.filter((todo) => todo.date > today && !todo.completed);
-  } else if (filterType === "expired") {
-    filteredTodoList = todoList.filter((todo) => todo.date < today && !todo.completed);
-  } else {
-    filteredTodoList = todoList;
-  }
-
+export function TodoList({ page, filterType }) {
+  const { filteredTodos } = useFilterTodos(filterType);
 
   return (
     <div className="flex justify-center items-start w-full mt-2">
       <ul className="w-4/5 max-w-[500px] list-none">
-        <ListMessage
-          todoList={todoList}
+        {/* <ListMessage
           page={page}
-          filteredTodoList={filteredTodoList}
-          filterType={filterType}
-        />
-        {filteredTodoList.map(task => (
+        /> */}
+        {filteredTodos.map(task => (
           <TodoItem
             key={task.id}
             task={task}
-            toggleTask={toggleTask}
-            deleteTask={deleteTask}
-            editTask={editTask}
           />
         ))}
       </ul>
