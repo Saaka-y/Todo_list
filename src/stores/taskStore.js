d973b6772e0115e353066bd5d403b04384b7477f
Sara <188415334+Saaka-y@users.noreply.github.com>
2025-12-07 15:28:37 +0900

    ♻️ remove check status from taskStore since it's managed on todoStore

--- src/stores/taskStore.js
+++ src/stores/taskStore.js
@@ -7,13 +7,11 @@ import { dayToString } from "@/utils/dayToString.js";
 export const useTaskStore = create((set, get) => ({
   selectedTaskId: null, // 編集中のタスクID
   currentTaskText: "", // 入力中のテキスト
-  currentStatus: false,
   currentTaskDate: null,  // 入力中の期日（Dateオブジェクト）
 
   // フォームに入力した値をセット
   setSelectedTaskId: (id) => set({ selectedTaskId: id }),
   setCurrentTaskText: (text) => set({ currentTaskText: text }),
-  setCurrentStatus: (bool) => set({ currentStatus: bool }),
   setCurrentTaskDate: (date) => set({ currentTaskDate: date }),
 
   // フォームをリセット
