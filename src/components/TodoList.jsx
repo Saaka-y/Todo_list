35bacdec16eec1c4c384542687835dfd426927e8
Sara <188415334+Saaka-y@users.noreply.github.com>
2025-12-07 14:59:08 +0900

    ♻️ 💄 update UI for headings on each todo list

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,5 +1,5 @@
 //@/components/TodoList.jsx
-import { ListMessage } from "@/components/ListMessage";
+import { ListHeading } from "@/components/ListHeading";
 import { TodoItem } from "@/components/TodoItem/TodoItem";
 import { useFilterTodos } from "@/hooks/useFilterTodos";
 
@@ -9,9 +9,11 @@ export function TodoList({ page, filterType }) {
   return (
     <div className="flex justify-center items-start w-full mt-2">
       <ul className="w-4/5 max-w-[500px] list-none">
-        {/* <ListMessage
+        <ListHeading
           page={page}
-        /> */}
+          filterType={filterType}
+          taskAmount={filteredTodos.length}
+        />
         {filteredTodos.map(task => (
           <TodoItem
             key={task.id}
